dc8d9001940c7652d3988c2beb918f88eb56e3bb
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-06-26 14:15:55 +0300

    Added to unit test to the library

--- BooksTask/test_book_functionality.py
+++ BooksTask/test_book_functionality.py
@@ -5,6 +5,26 @@ from book import Book
 
 class TestBookFunctionality(unittest.TestCase):
 
+    def test_init_book_function(self):
+        # Act
+        book1 = Book("Harry", "M7mod", 2001, "Magic")
+
+        # Assert
+        self.assertEqual("Harry", book1.title, "Title was not set correctly")
+        self.assertEqual("M7mod", book1.author, "Author was not set correctly")
+        self.assertEqual(2001, book1.publication_year, "Publication year was not set correctly")
+        self.assertEqual("Magic", book1.genre, "Genre was not set correctly")
+
+    def test_init_book_function_not_equal(self):
+        # Act
+        book1 = Book("Harry", "M7mod", 2001, "Magic")
+
+        # Assert
+        self.assertNotEqual(None, book1.title, "Title should not be None")
+        self.assertNotEqual(None, book1.author, "Author should not be None")
+        self.assertNotEqual(None, book1.publication_year, "Publication year should not be None")
+        self.assertNotEqual(None, book1.genre, "Genre should not be None")
+
     def test_update_book_function_equal(self):
         # Arrange
         book1 = Book("Harry", "M7mod", 2001, "Magic")
@@ -30,26 +50,6 @@ class TestBookFunctionality(unittest.TestCase):
         self.assertNotEqual("M7mod", book1.author, "Author should not be the old value")
         self.assertNotEqual(2001, book1.publication_year, "Publication year should not be the old value")
         self.assertNotEqual("Magic", book1.genre, "Genre should not be the old value")
-
-    def test_init_book_function(self):
-        # Act
-        book1 = Book("Harry", "M7mod", 2001, "Magic")
-
-        # Assert
-        self.assertEqual("Harry", book1.title, "Title was not set correctly")
-        self.assertEqual("M7mod", book1.author, "Author was not set correctly")
-        self.assertEqual(2001, book1.publication_year, "Publication year was not set correctly")
-        self.assertEqual("Magic", book1.genre, "Genre was not set correctly")
-
-    def test_init_book_function_not_equal(self):
-        # Act
-        book1 = Book("Harry", "M7mod", 2001, "Magic")
-
-        # Assert
-        self.assertNotEqual(None, book1.title, "Title should not be None")
-        self.assertNotEqual(None, book1.author, "Author should not be None")
-        self.assertNotEqual(None, book1.publication_year, "Publication year should not be None")
-        self.assertNotEqual(None, book1.genre, "Genre should not be None")
 
     def test_to_dict_function_equal(self):
         # Arrange
--- BooksTask/test_library_functionality.py
+++ BooksTask/test_library_functionality.py
@@ -1,4 +1,65 @@
 import unittest
 
+from book import Book
+from library import Library
+
+
 class TestLibraryFunctionality(unittest.TestCase):
+
+    def test_init_library_default_name_equal(self):
+        # Arrange
+        library = Library()
+
+        # Assert
+        self.assertEqual(library.filename, "library.json", "Default filename is incorrect")
+        self.assertListEqual(library.books, [], "Books list should be initialized as an empty list")
+
+    def test_init_library_custom_name(self):
+        # Arrange
+        library = Library("custom_name")
+
+        # Assert
+        self.assertEqual(library.filename, "custom_name", "Default filename is incorrect")
+        self.assertListEqual(library.books, [], "Books list should be initialized as an empty list")
+
+    def test_add_book_function_in(self):
+        # Arrange
+        library = Library()
+        book = Book("Harry", "Bahaa", 2001, "Magic")
+
+        # Act
+        library.add_book(book)
+
+        # Assert
+        self.assertIn(book, library.books)
+
+    def test_add_book_function_not_in(self):
+        # Arrange
+        book1 = Book("Harry", "Bahaa", 2001, "Magic")
+        book2 = Book("Harry", "Bahaa", 2001, "Magic")
+        library = Library()
+
+        # Act
+        library.add_book(book1)
+
+        # Assert
+        self.assertNotIn(book2, library.books)
+
+    def test_list_books(self):
+        # Arrange
+        library = Library()
+        book1 = Book("Harry", "Bahaa", 2001, "Magic")
+        book2 = Book("Potter", "Shibel", 2001, "Fantasy")
+        library.add_book(book1)
+        library.add_book(book2)
+
+        expected_list_books = [book1, book2]
+
+        # Act
+        actual_list_books = library.list_books()
+
+        # Assert
+        self.assertEqual(expected_list_books, actual_list_books)
+
+    def test_edit_book(self):
 

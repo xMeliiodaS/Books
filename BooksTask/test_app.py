b78a7845a571dbdd3eb8b16b0fe13632ab036339
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-06-28 22:15:03 +0300

    Modified code

--- BooksTask/test_app.py
+++ BooksTask/test_app.py
@@ -1,9 +1,9 @@
 import json
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
 
 
 # Run once before all tests
